85df9bd5e2e8b87301bcd9c2529dd927ca52d592
Donrashid90 <salimhashim_90@hotmail.com>
2025-11-09 16:07:15 +0100

    Add files via upload

---
+++ ForgeVerse_Static_Site/js/main.js
@@ -0,0 +1,5 @@
+const connectBtn = document.getElementById('connectBtn');
+connectBtn?.addEventListener('click', () => {
+  alert('Wallet connect coming soon. This is a visual demo button.');
+});
+function demoMint(){ alert('Mint demo clicked. Your actual launchpad/embed will go here.'); }
